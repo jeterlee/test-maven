ecb342f72f2ea18e2d275e87401695be38408082
Jeter Lee <xqlee120@yeah.net>
2017-12-01 13:12:13 +0800

    #
    #

---
+++ lib1/src/main/java/cn/jeterlee/lib1/lib1.java
@@ -0,0 +1,11 @@
+package cn.jeterlee.lib1;
+
+/**
+ * 测试库1
+ *
+ * @author jeterlee
+ * @date 2017/12/1 0001
+ * @email xqlee120@yeah.net
+ */
+public class lib1 {
+}
---
+++ lib2/src/main/java/cn/jeterlee/lib2/lib2.java
@@ -0,0 +1,11 @@
+package cn.jeterlee.lib2;
+
+/**
+ * 测试库2
+ *
+ * @author jeterlee
+ * @date 2017/12/1 0001
+ * @email xqlee120@yeah.net
+ */
+public class lib2 {
+}
